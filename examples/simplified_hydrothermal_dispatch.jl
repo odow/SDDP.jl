caa8529b9e4d53044498790d27b580f73a801fee
Thuener Silva <thuener@gmail.com>
2017-09-04 13:45:53 -0300

    Add header

--- examples/simplified_hydrothermal_dispatch.jl
+++ examples/simplified_hydrothermal_dispatch.jl
@@ -1,3 +1,9 @@
+#  Copyright 2017, Eyob Zewdie, Oscar Dowson
+#  This Source Code Form is subject to the terms of the Mozilla Public
+#  License, v. 2.0. If a copy of the MPL was not distributed with this
+#  file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#############################################################################
+
 # Simplified Hydrothermal Dispatch
 # $FCF(v_{t-1}) =$
 # \begin{array}{r l}
