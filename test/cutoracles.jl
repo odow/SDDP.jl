88aacb8880975e3e9d6e8c0f85103056672d6e4f
odow <odow003@aucklanduni.ac.nz>
2017-09-08 12:18:32 +1200

    Fix tests

--- test/cutoracles.jl
+++ test/cutoracles.jl
@@ -14,22 +14,49 @@ immutable NotACutOracle end
 @testset "Cut Oracles" begin
     @testset "New Cut Oracle" begin
         newcutoracle = NewCutOracle()
+        m = SDDPModel(
+            sense           = :Max,
+            stages          = 2,
+            objective_bound = 10,
+            cut_oracle = newcutoracle
+            ) do sp, t
+            @state(sp, x>=0, x0==0)
+            @noise(sp, w=1:2, x <= w)
+            @stageobjective(sp, x)
+        end
         cut = SDDP.Cut(1, [1])
-        @test_throws Exception SDDP.storecut!(newcutoracle, cut)
+        @test_throws Exception SDDP.storecut!(newcutoracle, m, SDDP.getsubproblem(m, 1, 1), cut)
         @test_throws Exception SDDP.validcuts(newcutoracle)
     end
 
     @testset "Not A Cut Oracle" begin
         notacutoracle = NotACutOracle()
-        cut = SDDP.Cut(1, [1])
-        @test_throws Exception SDDP.storecut!(notacutoracle, cut)
-        @test_throws Exception SDDP.validcuts(notacutoracle)
+        @test_throws Exception SDDPModel(
+            sense           = :Max,
+            stages          = 2,
+            objective_bound = 10,
+            cut_oracle = notacutoracle
+            ) do sp, t
+            @state(sp, x>=0, x0==0)
+            @noise(sp, w=1:2, x <= w)
+            @stageobjective(sp, x)
+        end
     end
 
     @testset "Default Cut Oracle" begin
         oracle = SDDP.DefaultCutOracle()
+        m = SDDPModel(
+            sense           = :Max,
+            stages          = 2,
+            objective_bound = 10,
+            cut_oracle = oracle
+            ) do sp, t
+            @state(sp, x>=0, x0==0)
+            @noise(sp, w=1:2, x <= w)
+            @stageobjective(sp, x)
+        end
         cut = SDDP.Cut(1, [1])
-        SDDP.storecut!(oracle, cut)
+        SDDP.storecut!(oracle, m, SDDP.getsubproblem(m, 1, 1), cut)
         @test SDDP.validcuts(oracle) == [cut]
     end
 end
