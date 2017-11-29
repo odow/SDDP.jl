0ba464205324c674099dfe2c81f721a1c132c0ae
LEAXPS-15\lkape <lkapelevich@yahoo.com.au>
2017-11-28 20:54:14 -0500

    update header

--- test/DRO.jl
+++ test/DRO.jl
@@ -1,4 +1,8 @@
-#  Copyright 2017, Lea Kapelevich
+#  Copyright 2017,  Oscar Dowson and contributors
+#  This Source Code Form is subject to the terms of the Mozilla Public
+#  License, v. 2.0. If a copy of the MPL was not distributed with this
+#  file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#############################################################################
 
 @testset "PopStd" begin
     x = rand(10)
