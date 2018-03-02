c2942f402b7700ce173a722333ffeabf550ce5a0
Oscar Dowson <odow@users.noreply.github.com>
2018-03-02 18:29:28 +1300

    Auto-build documentation (#103)
    * [ci-skip] add documenter to travis
    
    * Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -12,3 +12,12 @@ makedocs(
     ],
     assets = ["assets/custom.css", "3d.gif"]
 )
+
+deploydocs(
+    repo   = "github.com/odow/SDDP.jl.git",
+    target = "build",
+    osname = "linux",
+    julia  = "0.6",
+    deps   = nothing,
+    make   = nothing,
+)
