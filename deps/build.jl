82f83ee189b095a87ef88358a950dd69258bfcfb
odow <o.dowson@gmail.com>
2019-04-03 14:45:50 -0500

    Add `@stdlib`

--- deps/build.jl
+++ deps/build.jl
@@ -1,3 +1,19 @@
-import Pkg
+#  Copyright 2017-19, Oscar Dowson.
+#  This Source Code Form is subject to the terms of the Mozilla Public
+#  License, v. 2.0. If a copy of the MPL was not distributed with this
+#  file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-Pkg.add(Pkg.PackageSpec(url="https://github.com/odow/MathOptFormat.jl"))
+# In this file we install some unregistered dependencies. This is a hack until
+# such time that Julia gets its act together.
+
+# For some reason we need to add this here.
+push!(LOAD_PATH, "@stdlib")
+
+const UNREGISTERED_URLS = [
+    "https://github.com/odow/MathOptFormat.jl"
+]
+
+import Pkg
+for url in UNREGISTERED_URLS
+    Pkg.add(Pkg.PackageSpec(url=url))
+end
