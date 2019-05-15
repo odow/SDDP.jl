f76e5def7e29627804f5911c5bda2d5123afaf40
odow <o.dowson@gmail.com>
2019-05-15 15:10:37 -0500

    Updates to example

--- examples/no_strong_duality.jl
+++ examples/no_strong_duality.jl
@@ -1,17 +1,27 @@
-using SDDP, GLPK
+#  Copyright 2017-19, Oscar Dowson.
+#  This Source Code Form is subject to the terms of the Mozilla Public
+#  License, v. 2.0. If a copy of the MPL was not distributed with this
+#  file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-graph = SDDP.Graph(
-    :root,
-    [:node],
-    [(:root => :node, 1.0), (:node => :node, 0.5)]
-)
+using SDDP, GLPK, Test
 
-model = SDDP.PolicyGraph(
-        graph, optimizer = with_optimizer(GLPK.Optimizer), 
-        lower_bound = 0.0) do sp, t
-    @variable(sp, x, SDDP.State, initial_value = 1.0)
-    @stageobjective(sp, x.out)
-    @constraint(sp, x.in == x.out)
+function no_strong_duality()
+    model = SDDP.PolicyGraph(
+            SDDP.Graph(
+                :root,
+                [:node],
+                [(:root => :node, 1.0), (:node => :node, 0.5)]
+            ),
+            optimizer = with_optimizer(GLPK.Optimizer),
+            lower_bound = 0.0) do sp, t
+        @variable(sp, x, SDDP.State, initial_value = 1.0)
+        @stageobjective(sp, x.out)
+        @constraint(sp, x.in == x.out)
+    end
+
+    SDDP.train(model, iteration_limit = 20)
+
+    @test SDDP.calculate_bound(model) ≈ 2.0 atol=1e-8
 end
 
-SDDP.train(model, iteration_limit = 20)
+no_strong_duality()
