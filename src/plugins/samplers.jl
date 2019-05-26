e762f625361510a4b3a9ea939e5b2d248ecd0170
rodripor <34354280+rodripor@users.noreply.github.com>
2019-05-25 21:52:23 -0300

    Add samplers.jl in plugins folder

---
+++ src/plugins/samplers.jl
@@ -0,0 +1,27 @@
+include("../user_interface.jl")
+using StatsBase
+
+abstract type AbstractBackwardPassSampler end
+
+struct CompleteSampler <: AbstractBackwardPassSampler end
+
+struct MonteCarloSampler <: AbstractBackwardPassSampler
+    number_of_samples ::Integer
+end
+
+sample_backward_noise_terms(::CompleteSampler, node) = node.noise_terms
+
+function sample_backward_noise_terms(mcs::MonteCarloSampler, node) where
+    sampled = Noise[]
+    terms = [noise.term for noise in node.noise_terms]
+
+    n_samples = min(length(node.noise_terms,mcs.number_of_samples))
+    sampled_t = sample(terms,n_samples)
+    u=unique(sampled_t)
+    d=Dict([(i,count(x->x==i,sampled_t)) for i in u])
+
+    for i in 1:length(u)
+        push!(sampled,Noise(u[i],1/d[u[i]]))
+    end
+    return sampled
+end
