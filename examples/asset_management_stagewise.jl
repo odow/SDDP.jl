4023ce23158259cbede3e540f35ac1fbaa250b44
odow <o.dowson@gmail.com>
2020-01-07 14:16:27 -0600

    Update copyright

--- examples/asset_management_stagewise.jl
+++ examples/asset_management_stagewise.jl
@@ -1,8 +1,7 @@
-#  Copyright 2018, Oscar Dowson
+#  Copyright 2017-20, Oscar Dowson
 #  This Source Code Form is subject to the terms of the Mozilla Public
 #  License, v. 2.0. If a copy of the MPL was not distributed with this
 #  file, You can obtain one at http://mozilla.org/MPL/2.0/.
-#############################################################################
 
 #==
 Modified version of the Asset Management problem taken from
--- examples/msppy_hydro_thermal.jl
+++ examples/msppy_hydro_thermal.jl
@@ -1,4 +1,4 @@
-#  Copyright 2019, Oscar Dowson, Lingquan Ding (@lingquant).
+#  Copyright 2019-20, Oscar Dowson, Lingquan Ding (@lingquant).
 #  This Source Code Form is subject to the terms of the Mozilla Public
 #  License, v. 2.0. If a copy of the MPL was not distributed with this
 #  file, You can obtain one at http://mozilla.org/MPL/2.0/.
