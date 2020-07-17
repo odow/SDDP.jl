8275a473f546289650cea7d4a55688389a3a1a66
odow <o.dowson@gmail.com>
2020-07-17 11:00:13 -0500

    Add support for changing variable bounds

--- src/Experimental.jl
+++ src/Experimental.jl
@@ -53,28 +53,14 @@ function _add_edges(
 end
 
 function _add_node_to_dict(dest::Dict, node::Node, node_name::String)
-    random_variables, realizations = String, Dict{String, Any}[]
-    for noise in node.noise_terms
-        support = Dict{String, Float64}()
-        node.parameterize(noise.term)
-        for x in all_variables(node.subproblem)
-            if is_fixed(x)
-                support[name(x)] = fix_value(x)
-            end
-        end
-        push!(
-            realizations,
-            Dict("probability" => noise.probability, "support" => support)
-        )
-        if length(realizations) == 1
-            random_variables = collect(keys(support))
-        end
-        @assert length(setdiff(random_variables, keys(support))) == 0
-    end
-    for x in random_variables
-        unfix(variable_by_name(node.subproblem, x))
-    end
-    added_variables = _reformulate_objective_uncertainty(
+    random_variables = String[]
+    realizations = Dict{String, Any}[
+        Dict{String, Any}(
+            "probability" => noise.probability,
+            "support" => Dict{String, Float64}()
+        ) for noise in node.noise_terms
+    ]
+    undo_reformulation = _reformulate_uncertainty(
         node, realizations, random_variables
     )
     dest[node_name] = Dict(
@@ -88,25 +74,71 @@ function _add_node_to_dict(dest::Dict, node::Node, node_name::String)
         "subproblem" => _subproblem_to_dict(node.subproblem),
         "realizations" => realizations,
     )
-    if length(added_variables) > 0
-        # Undo the reformulation in user-space.
-        delete(node.subproblem, added_variables)
-        set_objective_function(node.subproblem, node.stage_objective)
-    end
+    undo_reformulation()
     return
 end
 
-function _reformulate_objective_uncertainty(
-    node::Node, realizations, random_variables
+"""
+    _reformulate_uncertainty(
+        node::Node, realizations, random_variables
+    )
+
+Convert any lower and upper bounds than depend on the uncertainty into linear
+constraints with a random variable.
+
+Fixed variables are recorded as random variables, but no transformation is done.
+
+Given an affine stageobjective with stagewise independent uncertainty,
+reformulate into a quadratic stage objective by replacing the random
+coefficients with random decision variables.
+
+Return a function that undoes the reformulation when called with no arguments.
+"""
+function _reformulate_uncertainty(
+    node::Node, realizations::Vector, random_variables::Vector{String}
 )
-    # TODO(odow): handle quadratic objectives.
-    # First loop:
-    #   - build different objectives
-    #   - detect ones that change
+    # Storage for things that are changing.
+    bounds = Dict{VariableRef, Any}[]
+    lb = Set{VariableRef}()
+    ub = Set{VariableRef}()
+    fx = Set{VariableRef}()
     objectives = AffExpr[]
-    constant_changes, coefficient_changes = false, Set{VariableRef}()
+    constant_changes = false
+    coefficient_changes = Set{VariableRef}()
+
+    # Loop 1: Collect terms that are changing
     for noise in node.noise_terms
         node.parameterize(noise.term)
+
+        # Collect changing variable bounds.
+        bound = Dict{VariableRef, Any}()
+        for x in all_variables(node.subproblem)
+            l, u, f = -Inf, Inf, 0.0
+            if has_lower_bound(x)
+                l = lower_bound(x)
+            end
+            if has_upper_bound(x)
+                u = upper_bound(x)
+            end
+            if is_fixed(x)
+                f = fix_value(x)
+            end
+            if length(bounds) >= 1
+                if bounds[1][x].l != l
+                    push!(lb, x)
+                end
+                if bounds[1][x].u != u
+                    push!(ub, x)
+                end
+                if bounds[1][x].f != f
+                    push!(fx, x)
+                end
+            end
+            bound[x] = (l = l, u = u, f = f)
+        end
+        push!(bounds, bound)
+
+        # Collect changing objective terms.
         push!(objectives, convert(AffExpr, node.stage_objective))
         if length(objectives) > 1
             obj = objectives[end]
@@ -118,40 +150,97 @@ function _reformulate_objective_uncertainty(
             end
         end
     end
+
     added_variables = VariableRef[]
+    added_constraints = ConstraintRef[]
     objective = copy(node.stage_objective)
+
     # Reformulate a changing objective constant.
     if constant_changes
-        constant_term = @variable(node.subproblem)
-        push!(added_variables, constant_term)
-        set_name(constant_term, "_SDDPjl_random_objective_constant_")
-        push!(random_variables, "_SDDPjl_random_objective_constant_")
+        new_name = "_SDDPjl_random_objective_constant_"
+        y = _add_new_random_variable(
+            node, new_name, random_variables, added_variables
+        )
         for (r, o) in zip(realizations, objectives)
-            r["support"]["_SDDPjl_random_objective_constant_"] = o.constant
+            r["support"][new_name] = o.constant
         end
         objective.constant = 0.0
-        objective.terms[constant_term] = 1.0
+        objective.terms[y] = 1.0
     end
+
     # Reformulate changing objective coefficients.
     if length(coefficient_changes) > 0
         objective = convert(QuadExpr, objective)
         for x in coefficient_changes
-            coef_term = @variable(node.subproblem)
-            push!(added_variables, coef_term)
-            coef_name = "_SDDPjl_random_objective_$(name(x))_"
-            set_name(coef_term, coef_name)
-            push!(random_variables, coef_name)
+            new_name = "_SDDPjl_random_objective_$(name(x))_"
+            y = _add_new_random_variable(
+                node, new_name, random_variables, added_variables
+            )
             for (r, o) in zip(realizations, objectives)
-                r["support"][coef_name] = get(o.terms, x, 0.0)
+                r["support"][new_name] = get(o.terms, x, 0.0)
             end
             delete!.(Ref(objective.aff.terms), x)
-            add_to_expression!(objective, 1.0, coef_term, x)
+            add_to_expression!(objective, 1.0, y, x)
         end
     end
+
+    # Reformulate fixed variables.
+    for x in fx
+        for (realization, bound) in zip(realizations, bounds)
+            realization["support"][name(x)] = bound[x].f
+        end
+        push!(random_variables, name(x))
+        unfix(x)
+    end
+
+    # Reformulate lower bounded variables.
+    for x in lb
+        new_name = "_SDDPjl_lower_bound_$(name(x))_"
+        y = _add_new_random_variable(
+            node, new_name, random_variables, added_variables
+        )
+        c = @constraint(node.subproblem, x >= y)
+        push!(added_constraints, c)
+        delete_lower_bound(x)
+        for (realization, bound) in zip(realizations, bounds)
+            realization["support"][new_name] = bound[x].l
+        end
+    end
+
+    # Reformulate upper bounded variables.
+    for x in ub
+        new_name = "_SDDPjl_upper_bound_$(name(x))_"
+        y = _add_new_random_variable(
+            node, new_name, random_variables, added_variables
+        )
+        c = @constraint(node.subproblem, x <= y)
+        push!(added_constraints, c)
+        delete_upper_bound(x)
+        for (realization, bound) in zip(realizations, bounds)
+            realization["support"][new_name] = bound[x].u
+        end
+    end
+
     # Set the objective function to be written out.
     set_objective_function(node.subproblem, objective)
-    return added_variables
+
+    return () -> begin
+        delete(node.subproblem, added_variables)
+        delete.(Ref(node.subproblem), added_constraints)
+        set_objective_function(node.subproblem, node.stage_objective)
+        return
+    end
 end
+
+function _add_new_random_variable(
+    node, new_name, random_variables, added_variables
+)
+    y = @variable(node.subproblem, base_name = new_name)
+    push!(added_variables, y)
+    push!(random_variables, new_name)
+    return y
+end
+
 
 function _dict_diff_keys(
     x::AbstractDict{K, V}, y::AbstractDict{K, V}
@@ -238,7 +327,11 @@ function Base.read(io::IO, ::Type{PolicyGraph}; bound::Float64 = 1e6)
         end
         obj_sgn = objective_sense(sp) == model_sense ? 1 : -1
         objective_coefficients, objf = _convert_objective_function(
-            sp, String.(data["nodes"][node_name]["random_variables"])
+            sp,
+            convert(
+                Vector{String},
+                data["nodes"][node_name]["random_variables"]
+            )
         )
         parameterize(sp, Ω, P) do ω
             for (k, v) in ω
--- test/Experimental.jl
+++ test/Experimental.jl
@@ -29,45 +29,72 @@ function _create_model(minimization::Bool)
             [i = 1:N], s[i] <= x[i].in
             sum(s) <= d
         end)
-        SDDP.parameterize(sp, t == 1 ? [1] : 1:length(DEMAND)) do i
-            JuMP.fix(d, DEMAND[i])
+        SDDP.parameterize(sp, t == 1 ? [1] : 1:length(DEMAND)) do ω
+            JuMP.fix(d, DEMAND[ω])
+            set_upper_bound(s[1], 0.1 * ω)
+            set_lower_bound(x[1].out, ω)
             sgn = minimization ? 1.0 : -1.0
             @stageobjective(
-                sp, sgn * (sum(C[i] * x[i].out for i = 1:N) - S[i] * s[i] + i)
+                sp, sgn * (sum(C[i] * x[i].out for i = 1:N) - S[ω] * s[ω] + ω)
             )
         end
     end
     return model
 end
 
-@testset "Min: Read and write to file" begin
-    model = _create_model(true)
-    SDDP.write_to_file(model, "experimental.sof.json")
-    new_model = SDDP.read_from_file("experimental.sof.json")
-    set_optimizer(new_model, GLPK.Optimizer)
-    SDDP.train(new_model; iteration_limit = 50, print_level = 0)
-    set_optimizer(model, GLPK.Optimizer)
-    SDDP.train(model; iteration_limit = 50, print_level = 0)
-    @test isapprox(
-        SDDP.calculate_bound(model),
-        SDDP.calculate_bound(new_model);
-        atol = 1e-6
-    )
-end
+@testset "Roundtrips" begin
+    @testset "Min: Read and write to file" begin
+        base_model = _create_model(true)
+        set_optimizer(base_model, GLPK.Optimizer)
+        SDDP.train(base_model; iteration_limit = 50, print_level = 0)
 
-@testset "Max: Read and write to file" begin
-    model = _create_model(false)
-    SDDP.write_to_file(model, "experimental.sof.json")
-    new_model = SDDP.read_from_file("experimental.sof.json")
-    set_optimizer(new_model, GLPK.Optimizer)
-    SDDP.train(new_model; iteration_limit = 50, print_level = 0)
-    set_optimizer(model, GLPK.Optimizer)
-    SDDP.train(model; iteration_limit = 50, print_level = 0)
-    @test isapprox(
-        SDDP.calculate_bound(model),
-        SDDP.calculate_bound(new_model);
-        atol = 1e-6
-    )
-end
+        model = _create_model(true)
+        SDDP.write_to_file(model, "experimental.sof.json")
+        set_optimizer(model, GLPK.Optimizer)
+        SDDP.train(model; iteration_limit = 50, print_level = 0)
 
-rm("experimental.sof.json")
+        new_model = SDDP.read_from_file("experimental.sof.json")
+        set_optimizer(new_model, GLPK.Optimizer)
+        SDDP.train(new_model; iteration_limit = 50, print_level = 0)
+
+        @test isapprox(
+            SDDP.calculate_bound(base_model),
+            SDDP.calculate_bound(model);
+            atol = 1e-6
+        )
+
+        @test isapprox(
+            SDDP.calculate_bound(base_model),
+            SDDP.calculate_bound(new_model);
+            atol = 1e-6
+        )
+    end
+
+    @testset "Max: Read and write to file" begin
+        base_model = _create_model(false)
+        set_optimizer(base_model, GLPK.Optimizer)
+        SDDP.train(base_model; iteration_limit = 50, print_level = 0)
+
+        model = _create_model(false)
+        SDDP.write_to_file(model, "experimental.sof.json")
+        set_optimizer(model, GLPK.Optimizer)
+        SDDP.train(model; iteration_limit = 50, print_level = 0)
+
+        new_model = SDDP.read_from_file("experimental.sof.json")
+        set_optimizer(new_model, GLPK.Optimizer)
+        SDDP.train(new_model; iteration_limit = 50, print_level = 0)
+
+        @test isapprox(
+            SDDP.calculate_bound(base_model),
+            SDDP.calculate_bound(model);
+            atol = 1e-6
+        )
+
+        @test isapprox(
+            SDDP.calculate_bound(base_model),
+            SDDP.calculate_bound(new_model);
+            atol = 1e-6
+        )
+    end
+    rm("experimental.sof.json")
+end
