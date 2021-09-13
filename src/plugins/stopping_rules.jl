b5383d1a7e9bbbe39131c4477ad3d3ee569cbed2
Oscar Dowson <odow@users.noreply.github.com>
2021-09-14 11:01:53 +1200

    Update stopping_rules.jl

--- src/plugins/stopping_rules.jl
+++ src/plugins/stopping_rules.jl
@@ -146,10 +146,9 @@ function convergence_test(
         # No change in the bound. There are two possibilities:
         #  1) we haven't added enough cuts
         #  2) the problem was deterministic or myopic
-        return all(
-            l -> isapprox(l.bound, l.simulation_value, rule.tolerance),
-            log,
-        )
+        return all(log) do l
+            return isapprox(l.bound, l.simulation_value; atol = rule.tolerance)
+        end
     end
     for i in 1:rule.num_previous_iterations
         if abs(log[end-i].bound - log[end-i+1].bound) > rule.tolerance
