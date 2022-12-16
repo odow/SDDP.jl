6f342638187a8f1573b7f370f345fdf61f3d5677
odow <o.dowson@gmail.com>
2022-12-16 15:02:06 +1300

    Update

--- docs/src/examples/infinite_horizon_hydro_thermal.jl
+++ docs/src/examples/infinite_horizon_hydro_thermal.jl
@@ -54,7 +54,7 @@ function infinite_hydro_thermal(; cut_type)
     end
     SDDP.train(
         model;
-        time_limit = 2.0,
+        iteration_limit = 400,
         log_frequency = 100,
         sampling_scheme = SDDP.InSampleMonteCarlo(terminate_on_cycle = true),
         cycle_discretization_delta = 0.1,
