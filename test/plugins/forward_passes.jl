7af1ae885158c9af1d0c5ab7dfd276ff57722486
Oscar Dowson <odow@users.noreply.github.com>
2022-12-19 14:38:18 +1300

    Add include_last_node option to DefaultForwardPass (#547)

--- test/plugins/forward_passes.jl
+++ test/plugins/forward_passes.jl
@@ -166,6 +166,137 @@ function test_RiskAdjustedForwardPass()
     return
 end
 
+function test_DefaultForwardPass_cyclic()
+    graph = SDDP.LinearGraph(3)
+    SDDP.add_edge(graph, 3 => 1, 0.9)
+    model = SDDP.PolicyGraph(
+        graph;
+        sense = :Max,
+        upper_bound = 100.0,
+        optimizer = HiGHS.Optimizer,
+    ) do node, stage
+        @variable(node, x, SDDP.State, initial_value = 0.0)
+        @stageobjective(node, x.out)
+        SDDP.parameterize(node, stage * [1.5]) do ω
+            return JuMP.set_upper_bound(x.out, ω)
+        end
+    end
+    pass = SDDP.DefaultForwardPass()
+    options = SDDP.Options(
+        model,
+        Dict(:x => 1.0),
+        SDDP.InSampleMonteCarlo(terminate_on_cycle = true),
+        SDDP.CompleteSampler(),
+        SDDP.Expectation(),
+        0.0,
+        true,
+        SDDP.AbstractStoppingRule[],
+        (a, b) -> nothing,
+        0,
+        0.0,
+        SDDP.Log[],
+        IOBuffer(),
+        1,
+        pass,
+        SDDP.ContinuousConicDuality(),
+        x -> nothing,
+    )
+    forward_trajectory = SDDP.forward_pass(model, options, pass)
+    @test length(forward_trajectory.scenario_path) == 4
+    @test length(forward_trajectory.sampled_states) == 4
+    @test options.starting_states[1] == [Dict(:x => 4.5)]
+    @test isempty(options.starting_states[2])
+    @test isempty(options.starting_states[3])
+    return
+end
+
+function test_DefaultForwardPass_cyclic_include_last_node()
+    graph = SDDP.LinearGraph(3)
+    SDDP.add_edge(graph, 3 => 1, 0.9)
+    model = SDDP.PolicyGraph(
+        graph;
+        sense = :Max,
+        upper_bound = 100.0,
+        optimizer = HiGHS.Optimizer,
+    ) do node, stage
+        @variable(node, x, SDDP.State, initial_value = 0.0)
+        @stageobjective(node, x.out)
+        SDDP.parameterize(node, stage * [1.5]) do ω
+            return JuMP.set_upper_bound(x.out, ω)
+        end
+    end
+    pass = SDDP.DefaultForwardPass(include_last_node = false)
+    options = SDDP.Options(
+        model,
+        Dict(:x => 1.0),
+        SDDP.InSampleMonteCarlo(terminate_on_cycle = true),
+        SDDP.CompleteSampler(),
+        SDDP.Expectation(),
+        0.0,
+        true,
+        SDDP.AbstractStoppingRule[],
+        (a, b) -> nothing,
+        0,
+        0.0,
+        SDDP.Log[],
+        IOBuffer(),
+        1,
+        pass,
+        SDDP.ContinuousConicDuality(),
+        x -> nothing,
+    )
+    forward_trajectory = SDDP.forward_pass(model, options, pass)
+    @test length(forward_trajectory.scenario_path) == 3
+    @test length(forward_trajectory.sampled_states) == 3
+    @test options.starting_states[1] == [Dict(:x => 4.5)]
+    @test isempty(options.starting_states[2])
+    @test isempty(options.starting_states[3])
+    return
+end
+
+function test_DefaultForwardPass_acyclic_include_last_node()
+    graph = SDDP.LinearGraph(3)
+    model = SDDP.PolicyGraph(
+        graph;
+        sense = :Max,
+        upper_bound = 100.0,
+        optimizer = HiGHS.Optimizer,
+    ) do node, stage
+        @variable(node, x, SDDP.State, initial_value = 0.0)
+        @stageobjective(node, x.out)
+        SDDP.parameterize(node, stage * [1.5]) do ω
+            return JuMP.set_upper_bound(x.out, ω)
+        end
+    end
+    pass = SDDP.DefaultForwardPass(include_last_node = false)
+    options = SDDP.Options(
+        model,
+        Dict(:x => 1.0),
+        SDDP.InSampleMonteCarlo(terminate_on_cycle = true),
+        SDDP.CompleteSampler(),
+        SDDP.Expectation(),
+        0.0,
+        true,
+        SDDP.AbstractStoppingRule[],
+        (a, b) -> nothing,
+        0,
+        0.0,
+        SDDP.Log[],
+        IOBuffer(),
+        1,
+        pass,
+        SDDP.ContinuousConicDuality(),
+        x -> nothing,
+    )
+    forward_trajectory = SDDP.forward_pass(model, options, pass)
+    @test length(forward_trajectory.scenario_path) == 3
+    @test length(forward_trajectory.sampled_states) == 3
+    @test isempty(options.starting_states[1])
+    @test isempty(options.starting_states[2])
+    @test isempty(options.starting_states[3])
+    return
+end
+
 end  # module
 
 TestForwardPasses.runtests()
