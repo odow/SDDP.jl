62a26148c823923fa4133fc6c53a858c9228c33b
Oscar Dowson <odow@users.noreply.github.com>
2023-08-31 16:00:58 +1200

    [docs] update deterministic to stochastic example (#659)

--- docs/src/tutorial/example_reservoir.jl
+++ docs/src/tutorial/example_reservoir.jl
@@ -67,57 +67,62 @@ T = size(data, 1)
 model = Model(HiGHS.Optimizer)
 set_silent(model)
 
-# `x[t]`: the amount of water in the reservoir at the start of stage `t`:
+# `x_storage[t]`: the amount of water in the reservoir at the start of stage `t`:
 
 reservoir_max = 320.0
-@variable(model, 0 <= x[1:T+1] <= reservoir_max)
+@variable(model, 0 <= x_storage[1:T+1] <= reservoir_max)
 
-# We need an initial condition for `x[1]`. Fix it to 300 units:
+# We need an initial condition for `x_storage[1]`. Fix it to 300 units:
 
 reservoir_initial = 300
-fix(x[1], reservoir_initial; force = true)
+fix(x_storage[1], reservoir_initial; force = true)
 
-# `u[t]`: the amount of water to flow through the turbine in stage `t`:
+# `u_flow[t]`: the amount of water to flow through the turbine in stage `t`:
 
 flow_max = 12
-@variable(model, 0 <= u[i = 1:T] <= flow_max)
+@variable(model, 0 <= u_flow[1:T] <= flow_max)
 
-# `s[t]`: the amount of water to spill from the reservoir in stage `t`,
+# `u_spill[t]`: the amount of water to spill from the reservoir in stage `t`,
 # bypassing the turbine:
 
-@variable(model, 0 <= s[1:T])
+@variable(model, 0 <= u_spill[1:T])
 
-# `r[t]`: the amount of thermal generation in stage `t`:
-@variable(model, 0 <= r[1:T])
+# `u_thermal[t]`: the amount of thermal generation in stage `t`:
 
-# `i[t]`: the amount of inflow to the reservoir in stage `t`:
+@variable(model, 0 <= u_thermal[1:T])
 
-@variable(model, i[1:T])
+# `ω_inflow[t]`: the amount of inflow to the reservoir in stage `t`:
+
+@variable(model, ω_inflow[1:T])
 
 # For this model, our inflow is fixed, so we fix it to the data we have:
 
 for t in 1:T
-    fix(i[t], data[t, :inflow])
+    fix(ω_inflow[t], data[t, :inflow])
 end
 
 # The water balance constraint says that the water in the reservoir at the start
 # of stage `t+1` is the water in the reservoir at the start of stage `t`, less
-# the amount flowed through the turbine, `u[t]`, less the amount spilled,
-# `s[t]`, plus the amount of inflow, `i[t]`, into the reservoir:
+# the amount flowed through the turbine, `u_flow[t]`, less the amount spilled,
+# `u_spill[t]`, plus the amount of inflow, `ω_inflow[t]`, into the reservoir:
 
-@constraint(model, [t = 1:T], x[t+1] == x[t] - u[t] - s[t] + i[t])
+@constraint(
+    model,
+    [t in 1:T],
+    x_storage[t+1] == x_storage[t] - u_flow[t] - u_spill[t] + ω_inflow[t],
+)
 
 # We also need a `supply = demand` constraint. In practice, the units of this
 # would be in MWh, and there would be a conversion factor between the amount of
 # water flowing through the turbine and the power output. To simplify, we assume
 # that power and water have the same units, so that one "unit" of demand is
-# equal to one "unit" of the reservoir `x[t]`:
+# equal to one "unit" of the reservoir `x_storage[t]`:
 
-@constraint(model, [t = 1:T], u[t] + r[t] == data[t, :demand])
+@constraint(model, [t in 1:T], u_flow[t] + u_thermal[t] == data[t, :demand])
 
 # Our objective is to minimize the cost of thermal generation:
 
-@objective(model, Min, sum(data[t, :cost] * r[t] for t in 1:T))
+@objective(model, Min, sum(data[t, :cost] * u_thermal[t] for t in 1:T))
 
 # Let's optimize and check the solution
 
@@ -131,38 +136,39 @@ objective_value(model)
 # Here's a plot of demand and generation:
 
 Plots.plot(data[!, :demand]; label = "Demand", xlabel = "Week")
-Plots.plot!(value.(r), label = "Thermal")
-Plots.plot!(value.(u), label = "Hydro")
+Plots.plot!(value.(u_thermal), label = "Thermal")
+Plots.plot!(value.(u_flow), label = "Hydro")
 
 # And here's the storage over time:
 
-Plots.plot(value.(x); label = "Storage", xlabel = "Week")
+Plots.plot(value.(x_storage); label = "Storage", xlabel = "Week")
 
 # ## Deterministic SDDP model
 
 # For the next step, we show how to decompose our JuMP model into SDDP.jl. It
-# should obtain the same solution!
+# should obtain the same solution.
 
 model = SDDP.LinearPolicyGraph(
     stages = T,
     sense = :Min,
     lower_bound = 0.0,
     optimizer = HiGHS.Optimizer,
-) do subproblem, t
+) do sp, t
     @variable(
-        subproblem,
-        0 <= x <= reservoir_max,
+        sp,
+        0 <= x_storage <= reservoir_max,
         SDDP.State,
         initial_value = reservoir_initial,
     )
-    @variable(subproblem, 0 <= u <= flow_max)
-    @variable(subproblem, 0 <= r)
-    @variable(subproblem, 0 <= s)
-    @variable(subproblem, i)
-    fix(i, data[t, :inflow])
-    @constraint(subproblem, x.out == x.in - u - s + i)
-    @constraint(subproblem, u + r == data[t, :demand])
-    @stageobjective(subproblem, data[t, :cost] * r)
+    @variable(sp, 0 <= u_flow <= flow_max)
+    @variable(sp, 0 <= u_thermal)
+    @variable(sp, 0 <= u_spill)
+    @variable(sp, ω_inflow)
+    fix(ω_inflow, data[t, :inflow])
+    @constraint(sp, x_storage.out == x_storage.in - u_flow - u_spill + ω_inflow)
+    @constraint(sp, u_flow + u_thermal == data[t, :demand])
+    @stageobjective(sp, data[t, :cost] * u_thermal)
+    return
 end
 
 # Can you see how the JuMP model maps to this syntax? We have created a
@@ -173,11 +179,12 @@ end
 #
 # * We need to provide a lower bound for the objective function. Since our costs
 #   are always positive, a valid lower bound for the total cost is `0.0`.
-# * We define `x` as a state variable using `SDDP.State`. A state variable is
-#   any variable that flows through time, and for which we need to know the value
-#   of it in stage `t-1` to compute the best action in stage `t`. The state
-#   variable `x` is actually two decision variables, `x.in` and `x.out`, which
-#   represent `x[t]` and `x[t+1]` respectively.
+# * We define `x_storage` as a state variable using `SDDP.State`. A state
+#   variable is any variable that flows through time, and for which we need to
+#   know the value of it in stage `t-1` to compute the best action in stage `t`.
+#   The state variable `x_storage` is actually two decision variables,
+#   `x_storage.in` and `x_storage.out`, which represent `x_storage[t]` and
+#   `x_storage[t+1]` respectively.
 # * We need to use `@stageobjective` instead of `@objective`.
 
 # Instead of calling `JuMP.optimize!`, SDDP.jl uses a `train` method. With our
@@ -198,11 +205,14 @@ SDDP.calculate_bound(model)
 # simulation.
 
 # Since our model is deterministic, we need only 1 replication of the
-# simulation, and we want to record the values of the `x`, `u`, and `r`
-# variables:
+# simulation, and we want to record the values of the `x_storage`, `u_flow`, and
+# `u_thermal` variables:
 
-n_replications = 1
-simulations = SDDP.simulate(model, n_replications, [:x, :u, :r]);
+simulations = SDDP.simulate(
+    model,
+    1,  # Number of replications
+    [:x_storage, :u_flow, :u_thermal],
+);
 
 # The `simulations` vector is too big to show. But it contains one element for
 # each replication, and each replication contains one dictionary for each stage.
@@ -212,11 +222,11 @@ simulations = SDDP.simulate(model, n_replications, [:x, :u, :r]);
 
 simulations[1][10]
 
-# Let's grab the trace of the `r` and `u` variables in the first replication,
-# and then plot them:
+# Let's grab the trace of the `u_thermal` and `u_flow` variables in the first
+# replication, and then plot them:
 
-r_sim = [sim[:r] for sim in simulations[1]]
-u_sim = [sim[:u] for sim in simulations[1]]
+r_sim = [sim[:u_thermal] for sim in simulations[1]]
+u_sim = [sim[:u_flow] for sim in simulations[1]]
 
 Plots.plot(data[!, :demand]; label = "Demand", xlabel = "Week")
 Plots.plot!(r_sim, label = "Thermal")
@@ -224,10 +234,10 @@ Plots.plot!(u_sim, label = "Hydro")
 
 # Perfect. That's the same as we got before.
 
-# Now let's look at `x`. This is a little more complicated, because we need to
-# grab the outgoing value of the state variable in each stage:
+# Now let's look at `x_storage`. This is a little more complicated, because we
+# need to grab the outgoing value of the state variable in each stage:
 
-x_sim = [sim[:x].out for sim in simulations[1]]
+x_sim = [sim[:x_storage].out for sim in simulations[1]]
 
 Plots.plot(x_sim; label = "Storage", xlabel = "Week")
 
@@ -245,25 +255,24 @@ model = SDDP.LinearPolicyGraph(
 ) do sp, t
     @variable(
         sp,
-        0 <= x <= reservoir_max,
+        0 <= x_storage <= reservoir_max,
         SDDP.State,
         initial_value = reservoir_initial,
     )
-    @variable(sp, 0 <= u <= flow_max)
-    @variable(sp, 0 <= r)
-    @variable(sp, 0 <= s)
-    @variable(sp, i)
+    @variable(sp, 0 <= u_flow <= flow_max)
+    @variable(sp, 0 <= u_thermal)
+    @variable(sp, 0 <= u_spill)
+    @variable(sp, ω_inflow)
     ## <--- This bit is new
-    Ω = [data[t, :inflow] - 2, data[t, :inflow], data[t, :inflow] + 5]
-    P = [0.3, 0.4, 0.3]
+    Ω, P = [-2, 0, 5], [0.3, 0.4, 0.3]
     SDDP.parameterize(sp, Ω, P) do ω
-        fix(i, ω)
+        fix(ω_inflow, data[t, :inflow] + ω)
         return
     end
     ## --->
-    @constraint(sp, x.out == x.in - u - s + i)
-    @constraint(sp, u + r == data[t, :demand])
-    @stageobjective(sp, data[t, :cost] * r)
+    @constraint(sp, x_storage.out == x_storage.in - u_flow - u_spill + ω_inflow)
+    @constraint(sp, u_flow + u_thermal == data[t, :demand])
+    @stageobjective(sp, data[t, :cost] * u_thermal)
     return
 end
 
@@ -277,14 +286,14 @@ SDDP.train(model; iteration_limit = 100)
 # Now simulate the policy. This time we do 100 replications because the policy
 # is now stochastic instead of deterministic:
 
-n_replications = 100
-simulations = SDDP.simulate(model, n_replications, [:x, :u, :r, :i]);
+simulations =
+    SDDP.simulate(model, 100, [:x_storage, :u_flow, :u_thermal, :ω_inflow]);
 
 # And let's plot the use of thermal generation in each replication:
 
 plot = Plots.plot(data[!, :demand]; label = "Demand", xlabel = "Week")
 for simulation in simulations
-    Plots.plot!(plot, [sim[:r] for sim in simulation], label = "")
+    Plots.plot!(plot, [sim[:u_thermal] for sim in simulation], label = "")
 end
 plot
 
@@ -294,13 +303,13 @@ plot
 
 plot = SDDP.SpaghettiPlot(simulations)
 SDDP.add_spaghetti(plot; title = "Storage") do sim
-    return sim[:x].out
+    return sim[:x_storage].out
 end
 SDDP.add_spaghetti(plot; title = "Hydro") do sim
-    return sim[:u]
+    return sim[:u_flow]
 end
 SDDP.add_spaghetti(plot; title = "Inflow") do sim
-    return sim[:i]
+    return sim[:ω_inflow]
 end
 SDDP.plot(
     plot,
@@ -334,10 +343,10 @@ SDDP.plot(
 # ```julia
 # if t == 52
 #     @variable(sp, terminal_cost_function >= 0)
-#     @constraint(sp, terminal_cost_function >= reservoir_initial - x.out)
-#     @stageobjective(sp, data[t, :cost] * r + terminal_cost_function)
+#     @constraint(sp, terminal_cost_function >= reservoir_initial - x_storage.out)
+#     @stageobjective(sp, data[t, :cost] * u_thermal + terminal_cost_function)
 # else
-#     @stageobjective(sp, data[t, :cost] * r)
+#     @stageobjective(sp, data[t, :cost] * u_thermal)
 # end
 # ```
 
