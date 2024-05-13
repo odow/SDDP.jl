dccf1aa186befea060f1e7c9b1f6ba455604795c
arthur-brigatto <103693830+arthur-brigatto@users.noreply.github.com>
2024-05-13 20:09:58 -0300

    Add method sample_backward_noise_terms_with_state

--- src/plugins/headers.jl
+++ src/plugins/headers.jl
@@ -116,9 +116,27 @@ abstract type AbstractBackwardSamplingScheme end
     )::Vector{Noise}
 
 Returns a `Vector{Noise}` of noises sampled from `node.noise_terms` using
-`backward_sampling_scheme`
+`backward_sampling_scheme`.
 """
 function sample_backward_noise_terms end
+
+"""
+    sample_backward_noise_terms_with_state(
+        sampler::AbstractBackwardSamplingScheme,
+        node::Node,
+        state::Dict{Symbol,Float64},
+    )::Vector{Noise}
+
+Returns a `Vector{Noise}` of noises sampled conditionally on the sate `state` using
+`backward_sampling_scheme`.
+"""
+function sample_backward_noise_terms_with_state(
+    sampler::AbstractBackwardSamplingScheme,
+    node::Node,
+    state::Dict{Symbol,Float64},
+)
+    return sample_backward_noise_terms(sampler, node)
+end
 
 # =========================== duality_handlers =========================== #
 
