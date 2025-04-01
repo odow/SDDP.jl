84cb8b8ced2f3ed6132a49b056e8737280610c90
odow <o.dowson@gmail.com>
2025-04-01 15:39:48 +1300

    Update

--- test/plugins/parallel_schemes.jl
+++ test/plugins/parallel_schemes.jl
@@ -20,7 +20,10 @@ procs = Distributed.addprocs(4)
 # As hack, run the following script:
 @everywhere begin
     import Pkg
-    Pkg.activate(".")
+    # @__FILE__         := test/plugins/parallel_schemes.jl
+    # @__DIR__          := test/plugins
+    # dirname(@__DIR__) := test
+    Pkg.activate(joinpath(dirname(@__DIR__), "Project.toml"))
 end
 
 @everywhere begin
