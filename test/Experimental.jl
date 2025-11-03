f04824f5866d694f92037d4f958cb2d1d46d388e
Oscar Dowson <o.dowson@gmail.com>
2025-11-04 11:12:11 +1300

    Update

--- test/Experimental.jl
+++ test/Experimental.jl
@@ -17,7 +17,7 @@ Downloads.download(
     "sof.schema.json",
 )
 const SCHEMA =
-    JSONSchema.Schema(JSON.parsefile("sof.schema.json"; use_mmap = false))
+    JSONSchema.Schema(JSON.parsefile("sof.schema.json"))
 
 function runtests()
     for name in names(@__MODULE__; all = true)
@@ -187,7 +187,7 @@ function test_write_kwarg()
         author = "Oscar Dowson",
         date = "1234-56-78",
     )
-    data = JSON.parsefile("experimental.sof.json"; use_mmap = false)
+    data = JSON.parsefile("experimental.sof.json")
     @test data["description"] == "Experimental model"
     @test data["author"] == "Oscar Dowson"
     @test data["date"] == "1234-56-78"
