3a7ec3d345f32cb2d65947fbbb73cf1244b5f7e4
Oscar Dowson <o.dowson@gmail.com>
2026-01-06 17:08:03 +1300

    Update

--- papers/policy_graph/inventory_management.jl
+++ papers/policy_graph/inventory_management.jl
@@ -1,6 +1,6 @@
 #  Copyright (c) 2017-26: Oscar Dowson and SDDP.jl contributors.
 #
-#  This  This Source Code Form is subject to the terms of the Mozilla Public
+#  This Source Code Form is subject to the terms of the Mozilla Public
 #  License, v. 2.0. If a copy of the MPL was not distributed with this
 #  file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
