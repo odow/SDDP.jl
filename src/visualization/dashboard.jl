ab5fab72bba80c0f09b2eb22cc61ca4cdcc9f1b5
Oscar Dowson <o.dowson@gmail.com>
2026-09-10 15:12:41 +1200

    Update to HTTP@2

--- src/visualization/dashboard.jl
+++ src/visualization/dashboard.jl
@@ -5,41 +5,34 @@
 # one at http://mozilla.org/MPL/2.0/.
 
 function launch_websocket(server_to_client::Channel{Log})
-    host, port = HTTP.ip"127.0.0.1", 8000
-    server = HTTP.Sockets.listen(host, port)
-    HTTP.WebSockets.listen!(host, port; server = server) do ws
-        for msg in ws
-            break  # Wait for the first message, then exit the loop
-        end
-        while true
-            if !isready(server_to_client)
-                sleep(0.01)
-                continue
+    return HTTP.serve!("127.0.0.1", 8000) do req::HTTP.Request
+        return HTTP.sse_stream(200) do stream
+            for log in server_to_client
+                data = Dict(
+                    "iteration" => log.iteration,
+                    "bound" => log.bound,
+                    "simulation" => log.simulation_value,
+                    "time" => log.time,
+                    "solves" => log.total_solves,
+                )
+                event = HTTP.SSEEvent(JSON.json(data); event = "iteration")
+                write(stream, event)
             end
-            new_log = take!(server_to_client)
-            data = Dict(
-                "iteration" => new_log.iteration,
-                "bound" => new_log.bound,
-                "simulation" => new_log.simulation_value,
-                "time" => new_log.time,
-            )
-            HTTP.send(ws, JSON.json(data))
         end
-        return
     end
-    return server
 end
 
 function launch_dashboard()
     server_to_client = Channel{Log}(typemax(Int))
     server = launch_websocket(server_to_client)
     launch_file(joinpath(@__DIR__, "dashboard.html"))
-    return (log::Union{Log,Nothing}, close_flag::Bool) -> begin
+    function dashboard_callback(log::Union{Log,Nothing}, close_flag::Bool)
         if close_flag
-            close(server)
+            HTTP.forceclose(server)
         elseif log !== nothing
             put!(server_to_client, log)
         end
         return
     end
+    return dashboard_callback
 end
